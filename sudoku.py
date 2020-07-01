2d608e2a3d674bd375b57b1ed5251d2d66d0e428
Lucas <lukinhassaito2013@hotmail.com>
2020-07-01 02:19:48 -0300

    Code to read the input was changed into a loop.

--- sudoku.py
+++ sudoku.py
@@ -1,50 +1,36 @@
 """
+########## SUDOKU CHECKER ##########
 The input should be just like the example below:
 
-6 4 2 1 8 3 5 9 7
-3 1 9 5 2 7 6 8 4
-5 8 7 9 4 6 3 2 1
-8 9 4 2 6 5 7 1 3
-7 3 6 8 1 9 2 4 5
-2 5 1 7 3 4 9 6 8
-4 6 5 3 9 1 8 7 2
-1 7 8 6 5 2 4 3 9
-9 2 3 4 7 8 1 5 6
+    6 4 2 1 8 3 5 9 7
+    3 1 9 5 2 7 6 8 4
+    5 8 7 9 4 6 3 2 1
+    8 9 4 2 6 5 7 1 3
+    7 3 6 8 1 9 2 4 5
+    2 5 1 7 3 4 9 6 8
+    4 6 5 3 9 1 8 7 2
+    1 7 8 6 5 2 4 3 9
+    9 2 3 4 7 8 1 5 6
 Outputs: "Correct." or "Wrong."
 
 """
 
 start = print("Type your sudoku board:")
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-c = list(map(int, input().split()))
-d = list(map(int, input().split()))
-e = list(map(int, input().split()))
-f = list(map(int, input().split()))
-g = list(map(int, input().split()))
-h = list(map(int, input().split()))
-i = list(map(int, input().split()))
-
-rows = [a, b, c, d, e, f, g, h, i]
-
+rows = []
+for i in range(9):
+    rows.append(list(map(int, input().split())))
 
 columns = []
 for i in range(9):
     columns.append([
-        rows[0][i],
-        rows[1][i],
-        rows[2][i],
-        rows[3][i],
-        rows[4][i],
-        rows[5][i],
-        rows[6][i],
-        rows[7][i],
+        rows[0][i], rows[1][i],
+        rows[2][i], rows[3][i],
+        rows[4][i], rows[5][i],
+        rows[6][i], rows[7][i],
         rows[8][i]
         ])
 
-squares = []
-
-def squares_create():
+def squares_create(squares):
     bigcol1=[]
     bigcol2=[]
     bigcol3=[]
@@ -69,7 +55,8 @@ def squares_create():
     squares.append(bigcol3[9:18])
     squares.append(bigcol3[18:27])
 
-squares_create()
+squares = []
+squares_create(squares)
 
 
 def condition1(rows):
@@ -85,5 +72,3 @@ if (condition1(rows) == True and condition2(columns) == True and condition3(squa
     print("Correct.")
 else:
     print("Wrong.")
-    
-
